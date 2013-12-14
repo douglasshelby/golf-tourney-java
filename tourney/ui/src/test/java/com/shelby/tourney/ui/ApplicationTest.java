7502eb7382f33b3942a25569dfa89a74d0e6d291
douglasshelby <douglas.shelby.dev@gmail.com>
2013-12-14 17:19:58 -0600

    added ui project

---
+++ tourney/ui/src/test/java/com/shelby/tourney/ui/ApplicationTest.java
@@ -0,0 +1,20 @@
+package com.shelby.tourney.ui;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplicationTest 
+    extends TestCase
+{
+	/**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}

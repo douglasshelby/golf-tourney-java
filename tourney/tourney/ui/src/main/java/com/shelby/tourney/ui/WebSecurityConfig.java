a49136127695dfc758136f79dd0d0ba99879298a
douglasshelby <douglas.shelby.dev@gmail.com>
2014-01-26 17:22:18 -0600

    added hypersql for unit testing
    starting to work with security

--- tourney/tourney/ui/src/main/java/com/shelby/tourney/ui/WebSecurityConfig.java
+++ tourney/tourney/ui/src/main/java/com/shelby/tourney/ui/WebSecurityConfig.java
@@ -3,19 +3,19 @@ package com.shelby.tourney.ui;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated();
-        http
+                .antMatchers("/", "/home","/resources/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
             .formLogin()
                 .defaultSuccessUrl("/hello")
                 .loginPage("/login")
